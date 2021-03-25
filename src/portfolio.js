81b1bbd4067881cbebe63171fcb591356e12c43d
Sergio Marquez de Silva <semarque@microsoft.com>
2021-03-25 00:01:14 -0700

    var

--- src/portfolio.js
+++ src/portfolio.js
@@ -188,7 +188,7 @@ const workExperiences = {
 To know how to get github key look at readme.md */
 
 const openSource = {
-  githubConvertedToken: process.env.REACT_APP_GITHUB_TOKEN,
+  githubConvertedToken: REACT_APP_GITHUB_TOKEN,
   githubUserName: "samdsmx", // Change to your github username to view your profile in Contact Section.
   showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
   display: true // Set false to hide this section, defaults to true
